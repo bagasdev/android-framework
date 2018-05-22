ee8601781c371d924dab011e2a23bd9190e22981
Bagas Nasution <bagasnasution@gmail.com>
2018-05-22 14:35:54 +0700

    m

---
+++ src/main/java/portfolio/bagasnasution/android_framework/Main.java
@@ -0,0 +1,8 @@
+package portfolio.bagasnasution.android_framework;
+
+/**
+ * Created by bagas on 5/22/2018.
+ */
+
+public class Main {
+}
---
+++ src/main/java/portfolio/bagasnasution/android_framework/siteplan/SitePlanView.java
@@ -0,0 +1,31 @@
+package portfolio.bagasnasution.android_framework.siteplan;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by bagas on 5/22/2018.
+ */
+
+public class SitePlanView extends RelativeLayout {
+
+    public SitePlanView(Context context) {
+        super(context);
+    }
+
+    public SitePlanView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SitePlanView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SitePlanView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+}
